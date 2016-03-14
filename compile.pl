48612fb6df063c973725f8d01b7fd16e1124502d
Arthur Wolf <wolf.arthur@gmail.com>
2016-03-14 12:47:04 +0100

    initial commit

---
+++ compile.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+use strict;
+use WWW::Mechanize;
+use HTML::TreeBuilder;
+use File::Slurp;
+use utf8;
+
+# Get the template file
+my $template = read_file("templates/index.tpl");
+
+# Find and replace 
+my @replaced;
+for my $to_replace ( split("\n", $template) ){
+    if( $to_replace =~ m{script.*static} ){
+        my $url = HTML::TreeBuilder->new_from_content( $to_replace )->look_down( _tag => "script", src => qr/static/ )->attr("src");
+        print "found '$url'\n";
+        push @replaced, "<script type='text/javascript'>\n" . read_file("$url") . "\n</script>\n";
+    }elsif( $to_replace =~ m{link.*static.*stylesheet} ){
+        my $element = HTML::TreeBuilder->new_from_content( $to_replace )->look_down( _tag => "link", href => qr/static/ );
+        my $url = $element->attr("href");
+        print "found '$url'\n";
+        push @replaced, "<style rel='" . $element->attr("rel") . "'>\n" . read_file("$url") . "\n</style>\n";
+    }else{
+        push @replaced, $to_replace;
+    }
+}
+
+# Generate new file
+write_file( 'index.html', @replaced );
+write_file( 'devel.html', $template );
+
