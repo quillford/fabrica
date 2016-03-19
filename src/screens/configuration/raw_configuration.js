830eb890addbd4476ed37fa5eb8092d514e757ad
Arthur Wolf <wolf.arthur@gmail.com>
2016-03-19 23:09:52 +0100

    first raw configuration screen

--- src/screens/configuration/raw_configuration.js
+++ src/screens/configuration/raw_configuration.js
@@ -5,6 +5,35 @@ var RawConfigurationScreen = Screen.extend({
         // Display this screen
         this.display('raw_configuration_screen');
 
+        // Set up sections
+        this.sections = [
+            { name: "Robot",                selector: "## Robot",        description: "Basic motion" },
+            { name: "Extruder",             selector: "## Extruder",     description: "Extruder motors" }, 
+            { name: "Laser",                selector: "## Laser",        description: "Laser power control"},
+            { name: "Temperature control",  selector: "## Temperature",  description: "Temperature regulation, typically for hotends and heated beds"},
+            { name: "Switch",               selector: "## Switch",       description: "Map inputs/outputs and commands"},
+            { name: "Temperature switch",   selector: "## Temperaturesw",description: "Toggle outputs at given temperatures"},
+            { name: "Endstops",             selector: "## Endstops",     description: "Homing, endstops and limit switches"},
+            { name: "Z-probe",              selector: "## Z-probe",      description: "Probing, calibration and levelling"},
+            { name: "Panel",                selector: "## Panel",        description: "Panels and displays"},
+            { name: "Custom menus",         selector: "## Custom menus", description: "Custom menu entries"},
+            { name: "Network",              selector: "## Network",      description: "Ethernet and web interface"},
+            { name: "System",               selector: "## System",       description: "System settings" } 
+        ];
+
+        // Display sections in the array
+        this.html.find(".section_list .section_line").remove();
+        var line_template = this.html.find(".section_line");
+        var _that = this;
+        $.each( this.sections, function( index, line ){
+            var new_line = line_template.clone();
+            new_line.removeClass("hidden");
+            new_line.find(".section-button").text( line.name );
+            new_line.find(".section-description").text( line.description );
+            _that.html.find(".section_list").append(new_line);
+            console.log(new_line);
+        });   
+
         // Setup button clicks       
         //this.html.find(".btn-raw-configuration").off().click(function(){ fabrica.screens.move.enter(); });
     },
